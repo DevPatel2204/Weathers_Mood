e8d61beea669123217bedafc85226f7798531683
Dev Patel <dpatel222430@gmail.com>
2024-05-06 00:30:11 +0530

    API and UI remaining

---
+++ Manager/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  WeatherApp
+//
+//  Created by Dev Patel on 05/05/24.
+//
+
+import Foundation
---
+++ WeatherApp/Views/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  WeatherApp
+//
+//  Created by Dev Patel on 05/05/24.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
